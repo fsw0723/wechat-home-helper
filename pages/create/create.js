2677462e49a94a49568bab0090e93b3b38bea67f
fsw0723 <sfang@homeaway.com>
2017-08-29 16:51:59 +0800

    First commit

---
+++ pages/create/create.js
@@ -0,0 +1,13 @@
+var types = ['default', 'primary', 'warn']
+var pageObject = {
+  data: {
+    defaultSize: 'default',
+    primarySize: 'default',
+    warnSize: 'default',
+    disabled: false,
+    plain: false,
+    loading: false
+  }
+}
+
+Page(pageObject)
